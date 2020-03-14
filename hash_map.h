c35e836a806eeb413542026cc001f8d71511cc73
IlyaChukanov <61186233+IlyaChukanov@users.noreply.github.com>
2020-03-14 17:51:17 +0300

    Update hash_map.h

--- hash_map.h
+++ hash_map.h
@@ -2,35 +2,32 @@
 
 // Copyright 2020 Ilya Chukanov
 
-#include <iostream>
 #include <algorithm>
 #include <cmath>
 #include <vector>
 #include <list>
-#include <stdexcept>      
-
-using namespace std;
+#include <stdexcept>
 
 template <class KeyType, class ValueType, class Hash = std::hash<KeyType>>
 class HashMap {
  public:
-    using const_iterator =
-        typename list<pair<const KeyType, ValueType>>::const_iterator;
-    using iterator = typename list<pair<const KeyType, ValueType>>::iterator;
+    using KeyValuePair = std::pair<const KeyType, ValueType>;
+    using const_iterator = typename std::list<KeyValuePair>::const_iterator;
+    using iterator = typename std::list<KeyValuePair>::iterator;
 
-    HashMap(Hash hasherObj = Hash());
-
-    HashMap(initializer_list<pair<KeyType, ValueType>> list,
-            Hash hasherObj = Hash());
-
+    HashMap(Hash hasher_obj = Hash());
+    
+    HashMap(std::initializer_list<std::pair<KeyType, ValueType>> list,
+            Hash hasher_obj = Hash());
+            
     template <typename IteratorType>
-    HashMap(IteratorType begin, IteratorType end, Hash hasherObj = Hash());
+    HashMap(IteratorType begin, IteratorType end, Hash hasher_obj = Hash());
 
     Hash hash_function() const;
 
     bool empty() const;
 
-    void insert(pair<KeyType, ValueType> promoteElement);
+    void insert(std::pair<KeyType, ValueType> key_value_pair);
 
     iterator begin();
 
@@ -38,23 +35,25 @@ class HashMap {
 
     iterator end();
 
-    HashMap& operator=(HashMap const& element_position_in_object);
+    HashMap& operator=(HashMap const& other);
 
-    iterator find(KeyType sample_key_for_iterating);
+    iterator find(const KeyType key);
 
-    void erase(KeyType eraseElem);
+    void erase(const KeyType key);
 
     const_iterator begin() const;
 
     size_t size() const;
 
-    ValueType& operator[](KeyType sample_key_for_iterating);
+    ValueType& operator[](const KeyType key);
 
-    const ValueType& at(KeyType sample_key_for_iterating) const;
+    const ValueType& at(const KeyType key) const;
 
-    const_iterator find(KeyType sample_key_for_iterating) const;
+    const_iterator find(const KeyType key) const;
 
     void clear();
+
+    void DoubleSize();
 
  private:
     const size_t initialTableSize_ = 20;
@@ -63,39 +62,43 @@ class HashMap {
 
     size_t num_hash_table_elements_ = 0;
 
-    vector<list<typename list<pair<const KeyType, ValueType>>::iterator>>
-        hash_table_;
+    std::vector<std::list<iterator>> hash_table_;
 
-    list<pair<const KeyType, ValueType>> all_elements_;
+    std::list<KeyValuePair> all_elements_;
 
     Hash function_hash_number_;
+    
 };
+
 
 template <class KeyType, class ValueType, class Hash>
 void HashMap<KeyType, ValueType, Hash>::insert(
-    pair<KeyType, ValueType> promoteElement) {
-    size_t hash_table_cell_id = function_hash_number_(promoteElement.first) %
+    std::pair<KeyType, ValueType> key_value_pair) {
+    size_t hash_table_cell_id = function_hash_number_(key_value_pair.first) %
                                 hash_table_size_quantity_elements_;
     for (auto& element : hash_table_[hash_table_cell_id]) {
-        KeyType def_prom = element->first;
-        if (def_prom == promoteElement.first) {
+        if (element->first == key_value_pair.first) {
             return;
         }
     }
-    all_elements_.push_back(promoteElement);
-    auto Myiterator = all_elements_.end();
-    --Myiterator;
-    hash_table_[hash_table_cell_id].push_back(Myiterator);
-
+    all_elements_.push_back(key_value_pair);
+    hash_table_[hash_table_cell_id].push_back(std::prev(all_elements_.end()));
     ++num_hash_table_elements_;
 
+    DoubleSize();
+}
+
+template <class KeyType, class ValueType, class Hash>
+void HashMap<KeyType, ValueType, Hash>::DoubleSize() {
     if (hash_table_size_quantity_elements_ > num_hash_table_elements_ * 2) {
         return;
     }
-    list<pair<KeyType, ValueType>> position_of_scale;
-    for (auto& Iterator_for_find_pos : all_elements_) {
-        position_of_scale.push_back(make_pair(Iterator_for_find_pos.first,
-                                              Iterator_for_find_pos.second));
+    std::list<std::pair<KeyType, ValueType>> position_of_scale;
+    for (auto& key_value_pair : all_elements_) {
+        std::pair<KeyType, ValueType> include_one;
+        include_one.first = key_value_pair.first;
+        include_one.second = key_value_pair.second;
+        position_of_scale.push_back(include_one);
     }
     hash_table_.clear();
     all_elements_.clear();
@@ -108,10 +111,11 @@ void HashMap<KeyType, ValueType, Hash>::insert(
     position_of_scale.clear();
 }
 
+
 template <class KeyType, class ValueType, class Hash>
 void HashMap<KeyType, ValueType, Hash>::clear() {
-    for (auto& finderIter : all_elements_) {
-        size_t hashFunction = function_hash_number_(finderIter.first) %
+    for (auto& element : all_elements_) {
+        size_t hashFunction = function_hash_number_(element.first) %
                               hash_table_size_quantity_elements_;
         hash_table_[hashFunction].clear();
     }
@@ -120,15 +124,12 @@ void HashMap<KeyType, ValueType, Hash>::clear() {
 }
 
 template <class KeyType, class ValueType, class Hash>
-typename list<pair<const KeyType, ValueType>>::const_iterator
-HashMap<KeyType, ValueType, Hash>::find(
-    KeyType sample_key_for_iterating) const {
-    size_t hashFunc = function_hash_number_(sample_key_for_iterating) %
+auto HashMap<KeyType, ValueType, Hash>::find(KeyType key) const -> const_iterator {
+    size_t hashFunc = function_hash_number_(key) %
                       hash_table_size_quantity_elements_;
-    for (auto& fonderInter : hash_table_[hashFunc]) {
-        KeyType def_prom = fonderInter->first;
-        if (sample_key_for_iterating == def_prom) {
-            return fonderInter;
+    for (auto& It : hash_table_[hashFunc]) {
+        if (key == It->first) {
+            return It;
         }
     }
     return all_elements_.end();
@@ -136,35 +137,34 @@ HashMap<KeyType, ValueType, Hash>::find(
 
 template <class KeyType, class ValueType, class Hash>
 const ValueType& HashMap<KeyType, ValueType, Hash>::at(
-    KeyType sample_key_for_iterating) const {
-    auto iterator_for_find_key = find(sample_key_for_iterating);
+    KeyType key) const {
+    auto iterator_for_find_key = find(key);
     if (all_elements_.end() == iterator_for_find_key) {
-        throw out_of_range("");
+        throw std::out_of_range("");
     }
     return iterator_for_find_key->second;
 }
 
 template <class KeyType, class ValueType, class Hash>
 ValueType& HashMap<KeyType, ValueType, Hash>::operator[](
-    KeyType sample_key_for_iterating) {
-    auto iterator_for_find_key = find(sample_key_for_iterating);
+    KeyType key) {
+    auto iterator_for_find_key = find(key);
     if (iterator(all_elements_.end()) == iterator_for_find_key) {
-        insert({sample_key_for_iterating, ValueType()});
-        iterator_for_find_key = find(sample_key_for_iterating);
+        insert({key, ValueType()});
+        iterator_for_find_key = find(key);
     }
     return iterator_for_find_key->second;
 }
 
 template <class KeyType, class ValueType, class Hash>
-void HashMap<KeyType, ValueType, Hash>::erase(KeyType eraseElem) {
-    size_t Polynomial_hash =
-        function_hash_number_(eraseElem) % hash_table_size_quantity_elements_;
-    for (auto Myit = hash_table_[Polynomial_hash].begin();
-         Myit != hash_table_[Polynomial_hash].end(); ++Myit) {
-        KeyType def_prom = (*Myit)->first;
-        if (eraseElem == def_prom) {
-            all_elements_.erase(*Myit);
-            hash_table_[Polynomial_hash].erase(Myit);
+void HashMap<KeyType, ValueType, Hash>::erase(const KeyType key) {
+    size_t hash_table_cell =
+        function_hash_number_(key) % hash_table_size_quantity_elements_;
+    for (auto It = hash_table_[hash_table_cell].begin();
+         It != hash_table_[hash_table_cell].end(); ++It) {
+        if (key == (*It)->first) {
+            all_elements_.erase(*It);
+            hash_table_[hash_table_cell].erase(It);
             num_hash_table_elements_--;
             return;
         }
@@ -172,14 +172,12 @@ void HashMap<KeyType, ValueType, Hash>::erase(KeyType eraseElem) {
 }
 
 template <class KeyType, class ValueType, class Hash>
-typename list<pair<const KeyType, ValueType>>::iterator
-HashMap<KeyType, ValueType, Hash>::find(KeyType sample_key_for_iterating) {
-    size_t hashFunc = function_hash_number_(sample_key_for_iterating) %
+auto HashMap<KeyType, ValueType, Hash>::find(const KeyType key) -> iterator {
+    size_t hashFunc = function_hash_number_(key) %
                       hash_table_size_quantity_elements_;
-    for (auto& position_in_quant : hash_table_[hashFunc]) {
-        KeyType def_prom = position_in_quant->first;
-        if (sample_key_for_iterating == def_prom) {
-            return iterator(position_in_quant);
+    for (auto& It : hash_table_[hashFunc]) {
+        if (key == It->first) {
+            return iterator(It);
         }
     }
     return iterator(all_elements_.end());
@@ -187,37 +185,37 @@ HashMap<KeyType, ValueType, Hash>::find(KeyType sample_key_for_iterating) {
 
 template <class KeyType, class ValueType, class Hash>
 HashMap<KeyType, ValueType, Hash>& HashMap<KeyType, ValueType, Hash>::operator=(
-    HashMap<KeyType, ValueType, Hash> const& element_position_in_object) {
-    auto iterator_for_iterating = this;
-    if (&element_position_in_object == iterator_for_iterating) {
-        return *iterator_for_iterating;
+    HashMap<KeyType, ValueType, Hash> const& other) {
+    auto It = this;
+    if (&other == It) {
+        return *It;
     }
-    for (auto& finderIter : all_elements_) {
-        size_t hashFunction = function_hash_number_(finderIter.first) %
+    for (auto& element : all_elements_) {
+        size_t hashFunction = function_hash_number_(element.first) %
                               hash_table_size_quantity_elements_;
         hash_table_[hashFunction].clear();
     }
     all_elements_.clear();
     num_hash_table_elements_ = 0;
-    function_hash_number_ = element_position_in_object.hash_function();
-    hash_table_.resize(element_position_in_object.hash_table_size_quantity_elements_);
-    for (auto& foundIter : element_position_in_object) {
-        insert(foundIter);
+    function_hash_number_ = other.hash_function();
+    hash_table_.resize(other.hash_table_size_quantity_elements_);
+    for (auto& element_in_other : other) {
+        insert(element_in_other);
     }
-    return *iterator_for_iterating;
+    return *It;
 }
 
 template <class KeyType, class ValueType, class Hash>
-HashMap<KeyType, ValueType, Hash>::HashMap(Hash hasherObj)
-    : function_hash_number_(hasherObj) {
+HashMap<KeyType, ValueType, Hash>::HashMap(Hash hasher_obj)
+    : function_hash_number_(hasher_obj) {
     hash_table_size_quantity_elements_ = initialTableSize_;
     hash_table_.resize(hash_table_size_quantity_elements_);
 }
 
 template <class KeyType, class ValueType, class Hash>
 HashMap<KeyType, ValueType, Hash>::HashMap(
-    initializer_list<pair<KeyType, ValueType>> list, Hash hasherObj)
-    : function_hash_number_(hasherObj) {
+    std::initializer_list<std::pair<KeyType, ValueType>> list, Hash hasher_obj)
+    : function_hash_number_(hasher_obj) {
     hash_table_size_quantity_elements_ = initialTableSize_;
     hash_table_.resize(hash_table_size_quantity_elements_);
     for (auto& pos_in_list : list) {
@@ -228,8 +226,8 @@ HashMap<KeyType, ValueType, Hash>::HashMap(
 template <class KeyType, class ValueType, class Hash>
 template <typename IteratorType>
 HashMap<KeyType, ValueType, Hash>::HashMap(IteratorType begin, IteratorType end,
-                                           Hash hasherObj)
-    : function_hash_number_(hasherObj) {
+                                           Hash hasher_obj)
+    : function_hash_number_(hasher_obj) {
     hash_table_size_quantity_elements_ = initialTableSize_;
     hash_table_.resize(hash_table_size_quantity_elements_);
     for (; begin != end; ++begin) {
@@ -248,26 +246,23 @@ bool HashMap<KeyType, ValueType, Hash>::empty() const {
 }
 
 template <class KeyType, class ValueType, class Hash>
-typename list<pair<const KeyType, ValueType>>::iterator
-HashMap<KeyType, ValueType, Hash>::begin() {
+auto HashMap<KeyType, ValueType, Hash>::begin() -> iterator {
     return iterator(all_elements_.begin());
 }
 
 template <class KeyType, class ValueType, class Hash>
-typename list<pair<const KeyType, ValueType>>::const_iterator
-HashMap<KeyType, ValueType, Hash>::end() const {
+auto HashMap<KeyType, ValueType, Hash>::end() const -> const_iterator 
+{
     return all_elements_.end();
 }
 
 template <class KeyType, class ValueType, class Hash>
-typename list<pair<const KeyType, ValueType>>::iterator
-HashMap<KeyType, ValueType, Hash>::end() {
+auto HashMap<KeyType, ValueType, Hash>::end() -> iterator {
     return iterator(all_elements_.end());
 }
 
 template <class KeyType, class ValueType, class Hash>
-typename list<pair<const KeyType, ValueType>>::const_iterator
-HashMap<KeyType, ValueType, Hash>::begin() const {
+auto HashMap<KeyType, ValueType, Hash>::begin() const -> const_iterator {
     return all_elements_.begin();
 }
 
